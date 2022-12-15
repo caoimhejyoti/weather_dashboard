d3eb18ab40f9bca9426ea8371b65c69efc3f242b
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-15 21:42:12 +0800

    pseudo code created

---
+++ develop/js/script.js
@@ -0,0 +1,31 @@
+// user inputs information into search field
+ 
+// user selects search button
+
+// selecting search button triggers API information fetch request
+
+
+// previous search button is created within aside div
+
+
+// this previous button must be able to be clicked to redo the search request
+    // local storage needs to be used for this.
+
+
+
+// search results create new HTML elements
+    //[present] current day card across the top of the page
+    //[future] 5 day forcast is created below with each day being a card. 
+    // information required is date, temp (C), wind (KMPH) and humidity.
+
+
+
+
+
+
+
+
+// APIs
+// need to add geocoding
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={"73d12f90301263ae1498b68e5abab7e5"};
+// https://openweathermap.org/api/geocoding-api

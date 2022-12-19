861def37ad73e0cf10d5a466602f7982e381b590
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-19 17:32:05 +0800

    trying to resolve displayCurrentWeather bug

--- develop/js/script.js
+++ develop/js/script.js
@@ -12,14 +12,7 @@ let fiveDayContainerEl = document.querySelector(".five-day-container");
 // display variables
 let currentWeather = JSON.parse(localStorage.getItem("currentWeatherData"));
 console.log(currentWeather);
-let currentTemp = currentWeather.main.temp;
-console.log(currentTemp); //used for debugging
 
-let currentHumidity = currentWeather.main.humidity;
-console.log(currentHumidity); //used for debugging
-
-let currentWind = currentWeather.wind.speed;
-console.log(currentWind); //used for debugging
 
 
 // TODO: REVIEW: user inputs information into search field - WORKING!
@@ -28,7 +21,7 @@ console.log(currentWind); //used for debugging
     
     let location = locationInputEl.value.trim();
 
-    console.log(location + " formSubmitLocation Function"); //used for debugging
+    console.log(location + " (source = formSubmitLocation Function)"); //used for debugging
 
     if (location) {
         getLocationCurrentWeather(location);
@@ -39,7 +32,7 @@ console.log(currentWind); //used for debugging
         alert("Please enter a location");
     }
 
-    console.log("formSubmitLocation function is working"); //used for debugging
+    console.log("formSubmitLocation function is reading"); //used for debugging
  };
 
 
@@ -54,12 +47,9 @@ let getLocationCurrentWeather = function (locationSearched) {
         .then (function (response){
             response.json().then(function (data) {
                 console.log(data);
-
-
-
             if (response.ok) {
                 localStorage.setItem("currentWeatherData", JSON.stringify(data));
-                displayCurrentWeather(); //FIXME: causing an error
+                displayCurrentWeather(); 
             }else{
             alert("Error: " + response.statusText);
             }
@@ -68,7 +58,7 @@ let getLocationCurrentWeather = function (locationSearched) {
         .catch (function (error) {
             alert("Unable to connect to WeatherService");
         });
-    console.log("fetch apiURL is working");
+    console.log("fetch apiURL is reading");
 };
 // previous search button is created within aside div
 
@@ -83,35 +73,37 @@ let getLocationCurrentWeather = function (locationSearched) {
     //[future] 5 day forcast is created below with each day being a card. 
     // information required is date, temp (C), wind (KMPH) and humidity.
 
-let displayCurrnetWeather = function () {
+let displayCurrentWeather = function () {
+
+    let currentTemp = currentWeather.main.temp; //FIXME: causing a break in the code - needs something in the local storage to be able to read?
+    console.log(currentTemp); //used for debugging
+
+    let currentHumidity = currentWeather.main.humidity;
+    console.log(currentHumidity); //used for debugging
+
+    let currentWind = currentWeather.wind.speed;
+    console.log(currentWind); //used for debugging
 
     if (currentWeather.length === 0) {
         currentDayContainerEl.textContent = "Location not found";
         return;
-    }
-    console.log("weather is empty");
+        console.log("current weather is empty"); // used for debugging
+    }else{
 
-    let location = locationInputEl.value.trim();
+    let location = currentWeather.name;
 
-    console.log(location + " displayCurrentWeather Function"); //used for debugging
+    console.log(location + " (source = displayCurrentWeather Function)"); //used for debugging
     // locationSearchTerm.textContent = searchTerm;
 
-    for (var i = 0; i < weather.length; i++) {
+    let locationNameEL = document.createElement("h2");
+    
+    let titleEL = document.createElement("span");
+    titleEL.textContent = locationInputEl;
 
-        let locationNameEL = document.createElement("h2");
-        
-        let titleEL = document.createElement("span");
-        titleEL.textContent = locationInputEl;
+    locationNameEL.appendChild(titleEL);
 
-        locationNameEL.appendChild(titleEL);
-
-
-
-
-
-
-    };
     console.log("weather is not empty");
+    };
 };
 
 // user selects search button

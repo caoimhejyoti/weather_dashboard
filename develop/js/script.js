06d638145a8e9fc0f4245222dbceb913c2a04e94
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-19 11:56:14 +0800

    creating function for adding location info to form

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,6 +1,27 @@
+// global variables
+let apiURL = 'http://http://api.openweathermap.org/data/2.5/weather?q=';
+let apiKey = "73d12f90301263ae1498b68e5abab7e5";
+let locationInputEl = document.querySelector(".location");
+let previousSearchEl = document.querySelector(".previous-search");
+let locationSearchForm = document.querySelector(".location-search");
+
 // user inputs information into search field
- 
+ let formSubmitLocation = function (e) {
+    e.preventDefault();
+    
+    let location = locationInputEl.value.trim();
+
+    if (location) {
+        getLocationWeather(location);
+
+        previousSearchEl.textContent = " ";
+        locationInputEl.value = " ";
+    }else {
+        alert("Please enter a location");
+    }
+ };
 // user selects search button
+
 
 // selecting search button triggers API information fetch request
 
